9ea3df1b7183f7f39ca0c3b2c74b9b8fbcdfcb96
Keinix <zackcosborn@gmail.com>
2018-03-16 21:05:32 +0900

    work on subreddit activity

--- app/src/main/java/io/keinix/timesync/Activities/SubredditActivity.java
+++ app/src/main/java/io/keinix/timesync/Activities/SubredditActivity.java
@@ -2,17 +2,38 @@ package io.keinix.timesync.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.keinix.timesync.R;
+import io.keinix.timesync.reddit.model.SubReddit;
 
 public class SubredditActivity extends AppCompatActivity {
 
+    @BindView(R.id.subredditBannerDraweeView) SimpleDraweeView subredditBannerView;
+    @BindView(R.id.subredditIconDraweeView) SimpleDraweeView subredditIconDraweeView;
+    @BindView(R.id.subredditNameTextView) TextView subredditNameTextView;
+
     public static final String KEY_SUBREDDIT = "KEY_SUBREDDIT";
+
+    private SubReddit mSubReddit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subreddit);
+        ButterKnife.bind(this);
+        mSubReddit = getIntent().getParcelableExtra(KEY_SUBREDDIT);
+        bindSubredditView();
+    }
+
+    private void bindSubredditView() {
+        if (mSubReddit.getBannerImage() != null) subredditBannerView.setImageURI(mSubReddit.getBannerImage());
+        if (mSubReddit.getIconImg() !=null) subredditIconDraweeView.setImageURI(mSubReddit.getIconImg());
+        subredditNameTextView.setText(mSubReddit.getDisplayNamePrefixed());
     }
 
 }
