79b707a92199c47271fb74935f34785e19db81a1
Keinix <zackcosborn@gmail.com>
2018-03-17 10:41:23 +0900

    work on subredditActivity, added button

--- app/src/main/java/io/keinix/timesync/Activities/SubredditActivity.java
+++ app/src/main/java/io/keinix/timesync/Activities/SubredditActivity.java
@@ -2,6 +2,7 @@ package io.keinix.timesync.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.facebook.drawee.view.SimpleDraweeView;
@@ -16,7 +17,7 @@ public class SubredditActivity extends AppCompatActivity {
     @BindView(R.id.subredditBannerDraweeView) SimpleDraweeView subredditBannerView;
     @BindView(R.id.subredditIconDraweeView) SimpleDraweeView subredditIconDraweeView;
     @BindView(R.id.subredditNameTextView) TextView subredditNameTextView;
-
+    @BindView(R.id.subscribeButton) Button subscribeButton;
     public static final String KEY_SUBREDDIT = "KEY_SUBREDDIT";
 
     private SubReddit mSubReddit;
@@ -32,8 +33,12 @@ public class SubredditActivity extends AppCompatActivity {
 
     private void bindSubredditView() {
         if (mSubReddit.getBannerImage() != null) subredditBannerView.setImageURI(mSubReddit.getBannerImage());
-        if (mSubReddit.getIconImg() !=null) subredditIconDraweeView.setImageURI(mSubReddit.getIconImg());
+        if (mSubReddit.getIconImg() !=null) {
+            subredditIconDraweeView.setImageURI(mSubReddit.getIconImg());
+            subredditIconDraweeView.getDrawable().clearColorFilter();
+        }
         subredditNameTextView.setText(mSubReddit.getDisplayNamePrefixed());
+        if (mSubReddit.
     }
 
 }
